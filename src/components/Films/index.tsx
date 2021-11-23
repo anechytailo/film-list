ce36d88007b635a264d0cfb7d5cbf96ba7950d2d
Aliona Nechytailo <alionan@phmgmt.com>
2021-11-23 15:14:29 +0200

    try class component

--- src/components/Films/index.tsx
+++ src/components/Films/index.tsx
@@ -1,37 +1,57 @@
-import { FC, useState } from 'react'
-type Props = { films: string[], updateList: (item: string[]) => void }
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+type Props = { films: string[], updateList: (item: string[]) => void };
+type State = { editText: object };
 
-const Films: FC<Props> = ({ films, updateList }) => {
-  const deleteFilm = (i:number) => {
-    updateList(films.filter((_,index) => i !== index));
-  };
-  const [editText, setEditText] = useState({});
-  const handleEdit = (e: any) => {
-    setEditText({
-      ...editText,
-      [e.target.name]: e.target.value
+class Films extends React.Component<Props, State> {
+  constructor(props:Props) {
+    super(props);
+    this.state = {editText: {}};
+    this.editFilm = this.editFilm.bind(this);
+    this.deleteFilm = this.deleteFilm.bind(this);
+  }
+
+  componentDidMount() {
+    console.log('hello from react');
+  }
+  componentWillUnmount(){}
+
+  deleteFilm(i:number) {
+    this.props.updateList(this.props.films.filter((_,index) => i !== index))
+  }
+
+  editFilm(i:number) {
+    const tempFilms = [...this.props.films];
+    tempFilms[i] = this.state.editText[i];
+    this.props.updateList(tempFilms);
+  }
+
+  handleEdit(e:any) {
+    this.setState({
+      editText:{
+        [e.target.name]: e.target.value
+      }
     });
-  };
-  const editFilm = (i:number) => {
-    const tempFilms = [...films];
-    tempFilms[i] = editText[i];
-    updateList(tempFilms);
-  };
+  }
 
-  return <ul>{films.map((item,index) => 
-    <li key={index}>
-      {item}
-      <input type="text" placeholder="edit"
-            name={'' + index}
-            // @ts-ignore
-            value={editText.index}
-            onChange={(e)=>handleEdit(e)}/>
+  render() {
+    const films = this.props.films.map((item, index) => {
+      //const contentID = uuidv4();
+      return (<li key={index}>
+        {item}
+        <input type="text" 
+              placeholder="edit"
+              name={`${index}`}
+              value={this.state.editText.index}
+              onChange={(e)=>this.handleEdit(e)}/>
 
-      <span onClick={() => editFilm(index)}>{'\u2705'}</span>
-      <span onClick={() => deleteFilm(index)}>{'\u274C'}</span>
-    </li>)
-    }
-    </ul>
-};
+        <span onClick={() => this.editFilm(index)}>{'\u2705'}</span>
+        <span onClick={() => this.deleteFilm(index)}>{'\u274C'}</span>
+      </li>)
+    });
+  
+    return <ul>{films}</ul>
+  }
+}
 
 export default Films; // євген ковальчук
