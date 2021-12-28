a68f3fe0cc63ba95612aa0a10dcc631ff63f60b1
Aliona Nechytailo <alionan@phmgmt.com>
2021-12-28 11:25:05 +0200

    add styles using different approaches

--- src/components/AddFilm/index.tsx
+++ src/components/AddFilm/index.tsx
@@ -1,16 +1,36 @@
-import { FC, useState } from 'react'
-type Props = { addFilm: (t: string) => void }
+import { FC, useState } from 'react';
+type Props = { addFilm: (t: string) => void };
+import styles from './AddFilm.module.scss'
+import { createUseStyles } from 'react-jss';
 
 const AddFilm: FC<Props> = ({ addFilm }) => {
   const [inputText, setInputText] = useState('');
   const handleAdd = () => {
     addFilm(inputText);
     setInputText('');
-  }
-  return <>
-      <input type="text" value={inputText} onChange={(e)=>setInputText(e.target.value)}/>
-      <button title="Add a Film" onClick={handleAdd}>Add a Film</button>
+  };
+  const stylesUse = createUseStyles({
+    inputStyle: {
+      margin: '0 15px',
+      fontStyle: 'italic',
+    },
+  });
+  const classes = stylesUse();
+
+  return (
+    <>
+      <input
+        type='text'
+        placeholder='New Film'
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+        className={classes.inputStyle}
+      />
+      <button title='Add a Film' onClick={handleAdd} className={styles.addCTA}>
+        Add a Film
+      </button>
     </>
+  );
 };
 
 export default AddFilm;
--- src/components/Films/index.tsx
+++ src/components/Films/index.tsx
@@ -1,37 +1,56 @@
-import { FC, useState } from 'react'
-type Props = { films: string[], updateList: (item: string[]) => void }
+import { FC, useState } from 'react';
+import styles from './FilmsList.module.scss';
+import clsx from 'clsx'; // assign a class with condition `clsx(false && styles.li)`
+
+type Props = { films: string[]; updateList: (item: string[]) => void };
 
 const Films: FC<Props> = ({ films, updateList }) => {
-  const deleteFilm = (i:number) => {
-    updateList(films.filter((_,index) => i !== index));
+  const deleteFilm = (i: number) => {
+    updateList(films.filter((_, index) => i !== index));
   };
   const [editText, setEditText] = useState({});
   const handleEdit = (e: any) => {
     setEditText({
       ...editText,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
-  const editFilm = (i:number) => {
+  const editFilm = (i: number) => {
     const tempFilms = [...films];
+    // @ts-ignore
     tempFilms[i] = editText[i];
     updateList(tempFilms);
   };
 
-  return <ul>{films.map((item,index) => 
-    <li key={index}>
-      {item}
-      <input type="text" placeholder="edit"
-            name={'' + index}
-            // @ts-ignore
-            value={editText.index}
-            onChange={(e)=>handleEdit(e)}/>
+  const inputStyle = { marginLeft: '15px' };
+  const inputStylePlus = { fontStyle: 'italic' };
+  const testInlineStyle = { ...inputStyle, ...inputStylePlus };
 
-      <span onClick={() => editFilm(index)}>{'\u2705'}</span>
-      <span onClick={() => deleteFilm(index)}>{'\u274C'}</span>
-    </li>)
-    }
-    </ul>
+  return (
+    <>
+      <h1 className={styles.h1}>Films to Watch</h1>
+      <ul>
+        {films.map((item, index) => (
+          <li key={index} className={clsx(true && styles.li)}>
+            {item}
+            <input
+              type='text'
+              placeholder='edit'
+              name={`${index}`}
+              // @ts-ignore
+              value={editText.index}
+              onChange={(e) => handleEdit(e)}
+              style={testInlineStyle}
+            />
+            <span onClick={() => editFilm(index)} className={styles.editCTA}>
+              {'\u2705'}
+            </span>
+            <span onClick={() => deleteFilm(index)}>{'\u274C'}</span>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default Films; // євген ковальчук
